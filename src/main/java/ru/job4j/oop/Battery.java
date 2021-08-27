4248392c2f5c21a535a05765c4b2b575292bb864
LubaEremenko <eremenko.luba79@gmail.com>
2021-08-27 11:49:36 +0100

    1.8. Состояние объекта [#167388]fixx

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -3,15 +3,18 @@ package ru.job4j.oop;
 public class Battery {
     //создаем поле
     private int charge;
+    private int add;
 
     //создаем конструктор для инициализации нач состояния
     public Battery(int size) {
         this.charge = size;
+        this.add = size;
     }
+
     // создаем конструктор для увеличения заряда
     public void addCharge (Battery another) {
-        this.charge = this.charge + another.charge;
-        another.charge = 0;
+        another.charge = this.charge + this.add;
+        this.charge = 0;
 
     }
 
@@ -21,6 +24,6 @@ public class Battery {
 
         System.out.println("Первоначальный заряд батарии: " + before.charge + "%. Дополнительно зарядили на " + add.charge + "%.");
         before.addCharge(add);
-        System.out.println("Суммарный заряд батарии: " + before.charge + "%. Остаток: " + add.charge + "%.");
+        System.out.println("Суммарный заряд батарии: " + add.charge + "%. Остаток: " + before.charge + "%.");
     }
 }
