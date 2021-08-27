cd581c9ca38d640a47fbf525e29b598d52de134d
LubaEremenko <eremenko.luba79@gmail.com>
2021-08-27 20:40:29 +0100

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -1,14 +1,13 @@
 package ru.job4j.oop;
 
 public class DummyDic {
-    public String engToRus() {
-        String engl = "forever";
-        return engl;
+    public String engToRus(String engl) {
+        return  "Неизвествное слово: " + engl;
     }
-
+/*
     public static void main(String[] args) {
         DummyDic dd = new DummyDic();
         String word = dd.engToRus();
-        System.out.println("Неизвествное слово: " + word);
-    }
+
+    }*/
 }
--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -1,4 +1,33 @@
 package ru.job4j.oop;
 
+import static java.lang.StrictMath.sqrt;
+import static java.lang.StrictMath.pow;
 public class Point {
+    //создание полей для объекта х и у ВСЕ ПОЛЯ НАЧИНАЮТСЯ С PRIVATE
+    private int x;
+    private int y;
+
+    //создание конструктора для задания нач состояния
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+
+    }
+
+    //создаем метод distance, который будет принимать переменную Point
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public static void main(String[] args) {
+        //cоздаем объекты
+        Point a = new Point(0, 0);
+        Point b = new Point(8, 2);
+        //вызов метода distance
+        double dist = a.distance(b);
+        System.out.println(dist);
+
+    }
+
+
 }
