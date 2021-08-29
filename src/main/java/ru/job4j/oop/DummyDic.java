19ab644b57fa5f2945828213174355c577aefd82
LubaEremenko <eremenko.luba79@gmail.com>
2021-08-29 15:08:31 +0100

    2.1. Рефакторинг - Расстояние между точками. [#122587 #195742] tests

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -5,7 +5,7 @@ public class DummyDic {
         return  "Неизвествное слово: " + eng;
     }
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         DummyDic dd = new DummyDic();
         String word = dd.engToRus();
         System.out.println(word);
--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -1,17 +1,36 @@
 package ru.job4j.oop;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.describedAs;
 
 public class PointTest extends TestCase {
-
-    public void testDistance() {
+    @Test
+    public void testDistance2() {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
         double rsl = a.distance(b);
-        double expected = 8.246211251235321;
-        assertThat (rsl, closeTo(8, 0.001));
+        assertEquals (2, rsl, 0.01);
     }
+
+    @Test
+    public void testDistance15() {
+        Point a = new Point(2, 7);
+        Point b = new Point(-8, 19);
+        double rsl = a.distance(b);
+        assertEquals(15.62, rsl, 0.01);
+
+    }
+
+    @Test
+    public void testDistance1103() {
+        Point a = new Point(-125, -98);
+        Point b = new Point(85, 985);
+        double rsl = a.distance(b);
+        assertEquals(1103.17, rsl, 0.01 );
+    }
+
 }
