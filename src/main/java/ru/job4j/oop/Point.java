971421b147b6c2971142675415a2cf2ea9adcead
LubaEremenko <eremenko.luba79@gmail.com>
2021-08-30 20:00:09 +0100

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -2,32 +2,37 @@ package ru.job4j.oop;
 
 import static java.lang.StrictMath.sqrt;
 import static java.lang.StrictMath.pow;
+
 public class Point {
     //создание полей для объекта х и у ВСЕ ПОЛЯ НАЧИНАЮТСЯ С PRIVATE
-    private int x;
-    private int y;
+    private double x;
+    private double y;
+    private double z;
 
     //создание конструктора для задания нач состояния
-    public Point(int first, int second) {
+    public Point(double first, double second, double third) {
         this.x = first;
         this.y = second;
+        this.z = third;
 
     }
 
-    //создаем метод distance, который будет принимать переменную Point
-    public double distance(Point that) {
-        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    //создаем метод distance3d, который будет принимать переменную Point
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
     }
 
     public static void main(String[] args) {
         //cоздаем объекты
-        Point a = new Point(0, 0);
-        Point b = new Point(8, 2);
-        //вызов метода distance
-        double dist = a.distance(b);
+        Point a = new Point(0, 0, 8);
+        Point b = new Point(2, 2, 9);
+        Point z = new Point(7, 9, 9);
+
+        //вызов метода distance3d
+        double dist = a.distance3d(b.distance3d(z));
         System.out.println(dist);
 
     }
-
 
 }
--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -3,6 +3,8 @@ package ru.job4j.oop;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.security.cert.PolicyNode;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.describedAs;
@@ -10,27 +12,12 @@ import static org.hamcrest.Matchers.describedAs;
 public class PointTest extends TestCase {
     @Test
     public void testDistance2() {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        double rsl = a.distance(b);
+        Point a = new Point(0, 0, 5);
+        Point b = new Point(0, 2, 3);
+        Point z = new Point(8, 19, 45);
+        double rsl = a.distance3d(b.distance3d(z));
         assertEquals (2, rsl, 0.01);
     }
 
-    @Test
-    public void testDistance15() {
-        Point a = new Point(2, 7);
-        Point b = new Point(-8, 19);
-        double rsl = a.distance(b);
-        assertEquals(15.62, rsl, 0.01);
-
-    }
-
-    @Test
-    public void testDistance1103() {
-        Point a = new Point(-125, -98);
-        Point b = new Point(85, 985);
-        double rsl = a.distance(b);
-        assertEquals(1103.17, rsl, 0.01 );
-    }
 
 }
