9322df2641cd5b742c36d2c760bca236372de22d
LubaEremenko <eremenko.luba79@gmail.com>
2021-08-31 11:56:01 +0100

    4. Расстояние между точками в трехмерном пространстве. [#122643 #196829]fixed

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -5,32 +5,40 @@ import static java.lang.StrictMath.pow;
 
 public class Point {
     //создание полей для объекта х и у ВСЕ ПОЛЯ НАЧИНАЮТСЯ С PRIVATE
-    private double x;
-    private double y;
-    private double z;
+    private int x;
+    private int y;
+    private int z;
 
     //создание конструктора для задания нач состояния
-    public Point(double first, double second, double third) {
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+
+    }
+
+    public Point(int first, int second, int third) {
         this.x = first;
         this.y = second;
         this.z = third;
 
     }
+    //создаем метод distance, который будет принимать переменную Point
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
 
     //создаем метод distance3d, который будет принимать переменную Point
-
-    public double distance3d(Point that) {
+     public double distance3d(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
-    }
+     }
 
     public static void main(String[] args) {
         //cоздаем объекты
         Point a = new Point(0, 0, 8);
         Point b = new Point(2, 2, 9);
-        Point z = new Point(7, 9, 9);
 
         //вызов метода distance3d
-        double dist = a.distance3d(b.distance3d(z));
+        double dist = a.distance3d(b);
         System.out.println(dist);
 
     }
--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -10,14 +10,29 @@ import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.describedAs;
 
 public class PointTest extends TestCase {
+
     @Test
-    public void testDistance2() {
-        Point a = new Point(0, 0, 5);
-        Point b = new Point(0, 2, 3);
-        Point z = new Point(8, 19, 45);
-        double rsl = a.distance3d(b.distance3d(z));
-        assertEquals (2, rsl, 0.01);
+    public void testDistance15() {
+        Point a = new Point(2, 7);
+        Point b = new Point(-8, 19);
+        double rsl = a.distance(b);
+        assertEquals(15.62, rsl, 0.01);
     }
 
+    @Test
+    public void testDistance1103() {
+        Point a = new Point(-125, -98);
+        Point b = new Point(85, 985);
+        double rsl = a.distance(b);
+        assertEquals(1103.17, rsl, 0.01 );
+    }
+
+    @Test
+    public void testDistance3d() {
+        Point a = new Point(0, 0, 5);
+        Point b = new Point(0, 2, 3);
+        double rsl = a.distance3d(b);
+        assertEquals (2.82, rsl, 0.01);
+    }
 
 }
