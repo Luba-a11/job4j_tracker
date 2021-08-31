4211bdba98e7241700d2cd44cca9b24e47c1a37f
LubaEremenko <eremenko.luba79@gmail.com>
2021-08-31 20:16:06 +0100

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,6 +3,19 @@ package ru.job4j.tracker;
 public class Item {
     private int id;
     private String name;
+    //конструктор без параметров,
+    public Item(){
+
+    }
+    //конструктор с параметром String name,
+    public Item(String name) {
+        this.name = name;
+    }
+    //конструктор с параметрами int id и String name.
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public int getId() {
         return id;
